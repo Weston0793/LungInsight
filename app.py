043f89408a4a54dd13dc0e906017270ac807b172
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-08-17 18:17:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,29 +7,7 @@ from torchvision import models
 import torch.nn as nn
 import torch.nn.functional as F
 import cv2
-
-# Load Models
-class MultiClassMobileNetV2(nn.Module):
-    def __init__(self):
-        super(MultiClassMobileNetV2, self).__init__()
-        base_model = models.mobilenet_v2()
-        base_model.features[0][0] = nn.Conv2d(1, 32, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-        base_model.classifier[-1] = nn.Linear(in_features=1280, out_features=5)
-        self.base_model = base_model
-
-    def forward(self, x):
-        return self.base_model(x)
-
-class MultiClassMobileNetV3Small(nn.Module):
-    def __init__(self):
-        super(MultiClassMobileNetV3Small, self).__init__()
-        base_model = models.mobilenet_v3_small()
-        base_model.features[0][0] = nn.Conv2d(1, 16, kernel_size=(3, 3), stride=(2, 2), padding=(1, 1), bias=False)
-        base_model.classifier = nn.Linear(in_features=576, out_features=5)
-        self.base_model = base_model
-
-    def forward(self, x):
-        return self.base_model(x)
+from Models import MultiClassMobileNetV2, MultiClassMobileNetV3Small
 
 # Initialize Models
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
