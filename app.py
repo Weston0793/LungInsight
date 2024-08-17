392bf0237fe83b8ec0927a75eef5fa9d6542ccaa
Aba Lőrincz, MD, PhD, MSc <114341000+Weston0793@users.noreply.github.com>
2024-08-17 22:51:16 +0200

    Update app.py

--- app.py
+++ app.py
@@ -89,9 +89,9 @@ def overlay_rectangles(image, heatmap):
         left, top, right, bottom = find_largest_similar_rectangle(heatmap_half, x_index, y_index)
         
         # Convert coordinates to original image space
-        x1 = origin_x + left * (original_width // (2 * cms))
+        x1 = origin_x + left * (original_width // (1.8 * cms))
         y1 = top * (original_height // cms)
-        x2 = origin_x + right * (original_width // (2 * cms))
+        x2 = origin_x + right * (original_width // (1.8 * cms))
         y2 = bottom * (original_height // cms)
         
         # Draw the rectangle on the image
